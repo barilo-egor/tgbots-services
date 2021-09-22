8403ff4b87b875678286c2b3c5c5f55676a6b9c9
barilo <e.barilo@isida.by>
2021-09-22 14:42:40 +0300

    imports

--- src/main/java/by/barilo/bot/TestBot.java
+++ src/main/java/by/barilo/bot/TestBot.java
@@ -3,6 +3,8 @@ package by.barilo.bot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
 @PropertySource("classpath:bot.properties")
