2c0cc57eb5cab7e8c59c5fbe360c1691c818a817
barilo <e.barilo@isida.by>
2021-09-22 14:36:30 +0300

    init classes

---
+++ src/main/java/by/barilo/tgbotsservices/TgbotsServicesApplication.java
@@ -0,0 +1,13 @@
+package by.barilo.tgbotsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgbotsServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgbotsServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/barilo/tgbotsservices/TgbotsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.barilo.tgbotsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgbotsServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
